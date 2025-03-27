19d605caf8b410e584739d43b95aabec8962a51f
Joel Kouakou <kouakoujoel7@gmail.com>
2025-03-27 01:03:39 +0000

    modify home to add search options with API documentation

--- personel_management/management_api/views.py
+++ personel_management/management_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters import rest_framework as filter
@@ -29,21 +30,52 @@ def home(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+###############################################################################
+class MilitaryFilter(filters.FilterSet):
+    rank = filters.ChoiceFilter(choices=Military.RANK_CHOICES)
+    date_of_birth = filters.DateFilter(field_name='date_of_birth', lookup_expr='gte')
+    current_unit = filters.CharFilter(lookup_expr='icontains')
+    first_name = filters.CharFilter(lookup_expr='icontains')
+    last_name = filters.CharFilter(lookup_expr='icontains')
+    service_number = filters.CharFilter(lookup_expr='icontains')
+    graduation_school = filters.CharFilter(lookup_expr='icontains')
 
-
-class MilitaryFilter(filter.FilterSet):
-    first_name = filter.CharFilter(lookup_expr='icontains')
-    rank = filter.CharFilter(lookup_expr='icontains')
-    service_number = filter.CharFilter(lookup_expr='icontains')
-    
 
     class Meta:
         model = Military
-        fields = ['first_name', 'rank', 'service_number']
+        fields = ['rank', 'current_unit', 'first_name', 'last_name', 'service_number']
+
+class ChildrenFilter(filters.FilterSet):
+    date_of_birth = filters.DateFilter(field_name='date_of_birth', lookup_expr='gte')
+    gender = filters.ChoiceFilter(choices=[('M', 'Male'), ('F', 'Female')])
+    grade = filters.CharFilter(lookup_expr='icontains')
+    first_name = filters.CharFilter(lookup_expr='icontains')
+    last_name = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Children
+        fields = ['first_name','gender', 'grade', 'military']
+
+class DiplomaFilter(filters.FilterSet):
+    institution = filters.CharFilter(lookup_expr='icontains')
+    degree = filters.CharFilter(lookup_expr='icontains')
+    field_of_study = filters.CharFilter(lookup_expr='icontains')
+    graduation_date = filters.DateFilter(field_name='graduation_date', lookup_expr='gte')
+    diploma_number = filters.CharFilter(lookup_expr='icontains')
+    type_of_diploma = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Diploma
+        fields = ['institution', 'degree', 'field_of_study','diploma_number', 'type_of_diploma', 'military'] 
+##################################################################################################
 
 class MilitaryViewSet(viewsets.ModelViewSet):
     queryset = Military.objects.all()
     serializer_class = MilitarySerializer
+    filter_class = MilitaryFilter
+    search_fields = ['rank', 'current_unit', 'first_name', 'last_name', 'service_number']
+    ordering_fields = ['rank', 'first_name', 'last_name', 'service_number','current_unit']
+    ordering = ['last_name']
 
     @action(detail=True, methods= ['get'])
     def children(self, request, pk=None):
@@ -62,10 +94,18 @@ class MilitaryViewSet(viewsets.ModelViewSet):
 class ChildrenViewSet(viewsets.ModelViewSet):
     queryset = Children.objects.all()
     serializer_class = ChildrenSerializer
+    filter_class = ChildrenFilter
+    search_fields = ['first_name','gender', 'grade', 'military']
+    ordering_fields = ['first_name','last_name','gender', 'date_of_birth', 'gender', 'military']
+    ordering = ['last_name']
 
 class DiplomaViewSet(viewsets.ModelViewSet):
     queryset = Diploma.objects.all()
     serializer_class = DiplomaSerializer
+    filter_class = DiplomaFilter
+    search_fields = ['institution', 'degree', 'field_of_study','diploma_number', 'type_of_diploma', 'military']
+    ordering_fields = ['degree', 'field_of_study','diploma_number', 'type_of_diploma','institution']
+    ordering = ['last_name']
 
 ##################################################################
 
